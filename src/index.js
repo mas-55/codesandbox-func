1b53b2f9a50a363e42b0a1de6639d7a0bac14592
mas-55 <127859250+mas-55@users.noreply.github.com>
2023-03-16 14:28:15 +0900

    記法追加

--- src/index.js
+++ src/index.js
@@ -54,14 +54,184 @@
  * テンプレート文字列
  */
 
-const name = "mas";
-const age = 28;
-// 私の名前はmasです。年齢は28歳です。
+// const name = "mas";
+// const age = 28;
+// // 私の名前はmasです。年齢は28歳です。
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// // 従来の関数
+// // function func1(str) {
+// //   return str;
+// // }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// // アロー関数
+// // const func2 = (str) => {
+// //   return str;
+// // };
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// // const func3 = (num1, num2) => {
+// //   return num1 + num2;
+// // };
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(1, 3));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "Sam",
+//   age:"28",
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です`;
+// console.log(message2);
+
+// const myProfile = ['Sam', 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です`
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です`
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+// sayHello("Tom");
+
+/**
+ * スプレッド構文　...
+ */
+// // 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1); // [1, 2]
+// console.log(...arr1); // 1 2
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// // 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// // 直接参照渡しするとバグの温床になってしまう
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+// const nameArr = ["田中", "山田", "鈴木"];
+// // 従来の方法
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// // mapの例
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// })
+// console.log(nameArr2);
+
+// // mapを使用して1要素ずつ出力
+// nameArr.map((name) => console.log(name));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// // filterの例→条件合致するもので抽出
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 == 1;
+// });
+// console.log(newNumArr);
+
+// // mapを条件付きで使用
+// const newNameArr = nameArr.map((name) => {
+//   if (name == "鈴木") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// // ある条件 ? 条件がtrueの時　: 条件がfalseの時
+// const val1 = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// // 数値かどうかの判定
+// const num = 1300;
+// const formattedNum =
+//   typeof num === `number` ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// // 数値が許容範囲かを判定
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// // よくある使い方
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log('1か2はtrueになります');
+// }
+// if (flag1 && flag2) {
+//   console.log('1も2もtrueになります');
+// }
+
+// // ※JSXの中で論理演算子使うことがある
+// // || は左側がfalseとなるとき右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// // && は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
